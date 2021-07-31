51bcba6c786cdc7929ee9223ba63b8a83b4d0543
jatin7363 <jatin@websitetoolbox.com>
2021-07-31 18:12:39 +0530

    add email options

--- routes/files.js
+++ routes/files.js
@@ -12,19 +12,19 @@ let storage = multer.diskStorage({
   },
 })
 
-let upload = multer({ storage, limits:{ fileSize: 1000000 * 100 }, }).single('myfile') //100mb
+let upload = multer({ storage, limits: { fileSize: 1000000 * 100 }, }).single('myfile') //100mb
 
 router.post('/', (req, res) => {
   // store file
   upload(req, res, async (err) => { // upload's multer
     // validation request
     if (!req.file) {
-      return res.json({ error: 'File is required'})
+      return res.json({ error: 'File is required' })
     }
 
     if (err) {
-      // return res.status(500).send({ error: err.message })
-      return res.status(500), send({ error: err.message })
+      return res.status(500).send({ error: err.message })
+      // return res.status(500), send({ error: err.message })
     }
     // store into database
     const file = new File({
@@ -41,4 +41,41 @@ router.post('/', (req, res) => {
 
 })
 
+router.post('/send', async (req, res) => { // mail api
+  const { uuid, emailTo, emailFrom, expiresIn } = req.body
+  if (!uuid || !emailTo || !emailFrom) {
+    return res.status(422).send({ error: 'All fields are required except expiry.' })
+  }
+  // Get data from db 
+  try {
+    const file = await File.findOne({ uuid: uuid })
+    if (file.sender) {
+      return res.status(422).send({ error: 'Email already sent once.' })
+    }
+    file.sender = emailFrom
+    file.receiver = emailTo
+    const response = await file.save()
+    // send mail
+    const sendMail = require('../services/mailService')
+    sendMail({
+      from: emailFrom,
+      to: emailTo,
+      subject: `A File is shared by ${emailFrom}`,
+      text: `${emailFrom} shared a file with you.`,
+      html: require('../services/emailTemplate')({ // https://github.com/leemunroe/responsive-html-email-template
+        emailFrom: emailFrom,
+        downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}?source=email`,
+        size: parseInt(file.size / 1000) + ' KB',
+        expires: '24 hours'
+      })
+    }).then(() => {
+      return res.json({ success: true })
+    }).catch(err => {
+      return res.status(500).json({ error: 'Error in email sending.: '+ err })
+    })
+  } catch (err) {
+    return res.status(500).send({ error: 'Something went wrong.' })
+  }
+
+})
 module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express() // calling express
 const path = require('path')
-
 const PORT = process.env.PORT || 3000
+
 app.use(express.static('public'))
+app.use(express.json())
+
 const connectDB = require('./config/db')
 connectDB()
 
@@ -15,6 +17,7 @@ app.set('view engine', 'ejs') // html file ki extention kya h
 app.use('/api/files', require('./routes/files')) // if /api/files is use then send it to './routes/files'
 app.use('/files', require('./routes/show'))
 app.use('/files/download',require('./routes/download'))
+
 app.listen(PORT, () => {
   console.log(`Listening on port no. ${PORT}`)
 })
